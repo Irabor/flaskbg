173a717cd417739c5e5f645856f676d27bf05e0d
Irabor <fifemson@gmail.com>
2017-01-28 16:55:16 +0200

    ...

--- test.py
+++ test.py
@@ -1,10 +1,1 @@
 #!/usr/bin/env python
-import urllib2
-import re
-
-def page(url):
-    req = urllib2.Request(url)
-    page = urllib2.urlopen(req)
-    print(page.read())
-
-page('http://ciu.edu.tr/en')
